85ddbb9dcc1772b97299036f692781546d49312b
Adam Ryu <155268146+adavidryu@users.noreply.github.com>
2026-07-22 20:19:16 -0500

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # Smash two heaviest stones, x and y
+        # If values equal -> both destroyed
+        # If x value < y value -> x destroyed, y loses value and reorganized
+        # Repeat until one or no stones remain
+
+        h = []
+
+        for stone in stones:
+            heapq.heappush(h, -stone)
+        
+        while len(h) > 1:
+            y = -heapq.heappop(h)
+            x = -heapq.heappop(h)
+
+            if x < y:
+                heapq.heappush(h, -(y-x))
+        
+        if h:
+            return -h[0]
+        
+        return 0
